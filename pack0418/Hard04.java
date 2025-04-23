0c2df0190c1d68912adfb5c352f8892e6ffe0425
jonasondayo <sin0915tate@gmail.com>
2025-04-23 14:09:09 +0900

    Hard完成

---
+++ pack0418/Hard04.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+class Hard04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1辺の長さを入力:");
+        int x = sc.nextInt();
+        for (int $i=1; $i<=x; $i++) {
+            for (int $z=1; $z<=x; $z++) {
+                if ($z == $i || $z == x - $i + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
